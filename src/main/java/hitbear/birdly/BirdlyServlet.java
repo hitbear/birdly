06186b9b5cb108ca468272fae9de89b48ca199cd
mh <ma@mh-Lenovo>
2019-07-02 20:51:04 +0200

    encr

--- src/main/java/hitbear/birdly/BirdlyServlet.java
+++ src/main/java/hitbear/birdly/BirdlyServlet.java
@@ -1,7 +1,13 @@
 package hitbear.birdly;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,7 +33,10 @@ public class BirdlyServlet extends HttpServlet{
         resp.getWriter().println("EmbeddedJetty");
     }
 	
-	@Override
+	
+	//unencrypted case
+	/*
+	 * @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
  
@@ -41,5 +50,79 @@ public class BirdlyServlet extends HttpServlet{
 		if(req.getParameter("information2")!=null) {
 			fileWriter.writeToFile("/home/mh/Dokumente/information2.txt",req.getParameter("information2").toString());
 		}
+    }*/
+	
+	
+	
+	@SuppressWarnings("deprecation")
+	//encrypted case
+	@Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+		String encrypted_data = "";
+		String data = "";
+		if(req.getParameter("enc").compareTo("true") == 0) {
+			System.out.println("encrypted traffic");
+			if(req.getParameter("client") != null) {
+				System.out.println(req.getParameter("client"));
+				//System.out.println(req.getReader().toString());
+			}
+			if(req.getParameter("information")!=null) {
+				
+				//try to decrypt with private key
+				try {
+					//TODO: .replace(" ", "+"); is not nice...
+					encrypted_data = req.getParameter("information").toString().replace(" ", "+").replace("\n", "");
+					data = Decryptor.decrypt(encrypted_data);
+				} catch (InvalidKeyException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalBlockSizeException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (BadPaddingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NoSuchAlgorithmException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NoSuchPaddingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				fileWriter.writeToFile("/home/mh/Dokumente/information1.txt",data);
+			}
+			if(req.getParameter("information2")!=null) {
+				
+				//try to decrypt with private key
+				data = "";
+				try {
+					//TODO: .replace(" ", "+"); is not nice...
+					encrypted_data = req.getParameter("information2").toString().replace(" ", "+").replace("\n", "");
+					data = Decryptor.decrypt(encrypted_data);
+				} catch (InvalidKeyException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalBlockSizeException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (BadPaddingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NoSuchAlgorithmException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NoSuchPaddingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+				fileWriter.writeToFile("/home/mh/Dokumente/information2.txt",data);
+			}
+		}
+
+		
     }
 }
